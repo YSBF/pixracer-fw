73ea28bf5dfc1260dcd6c474768ee59b9cb29709
Michael Spieler <michael.spieler@epfl.ch>
2018-03-30 23:26:28 +0200

    Import mcucom library

---
+++ lib/mcucom/ts/serialization_msgpack.h
@@ -0,0 +1,31 @@
+#ifndef TS_SERIALIZATION_MSGPACK_H
+#define TS_SERIALIZATION_MSGPACK_H
+
+
+#include "type_definition.h"
+#include <cmp/cmp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool ts_serialize_msgpack(const void *var,
+                              const ts_type_definition_t *type,
+                              char *buf,
+                              size_t buf_sz);
+
+bool ts_serialize_msgpack_compact(const void *var,
+                                      const ts_type_definition_t *type,
+                                      char *buf,
+                                      size_t buf_sz);
+
+bool ts_cmp_ser_type(const void *var,
+                         const ts_type_definition_t *type,
+                         cmp_ctx_t *ctx,
+                         bool compact);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TS_SERIALIZATION_MSGPACK_H */
